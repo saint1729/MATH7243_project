6373963ea375797f5754c076ebc8414b1bd47bd9
solheim13 <ramsey.julia.s@gmail.com>
2022-04-13 21:11:14 -0400

    Work in progress - generalizing function/data load

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
+# importing os module
+import os
 
 
 def preprocess(image, label):
@@ -15,12 +17,39 @@ if __name__ == "__main__":
     mpl.rc('xtick', labelsize=12)
     mpl.rc('ytick', labelsize=12)
 
-    file_dir = "/Users/sakshisuman12/Desktop/MATH7243_project/unzipped/XN_project/renders_small/"
+    # User Specific Data Location
+    if (os.getlogin() == 'ramse'):
+        file_dir = "C:/Users/ramse/Documents/School/Northeastern/Machine Learning/Final Project/max_contrast_renders/"
+    else:
+        file_dir = "/Users/sakshisuman12/Desktop/MATH7243_project/unzipped/XN_project/renders_small/"
 
+    # All folders (must be in alphabetical order for keras loading function to work correctly)
+    # QUESTION: How are we sorting/classifying multi? Should we manually move those files into 
+    # other folders based on labels in the csv?
+    label_types = ['epidural', 'intraparenchymal', 'intraventricular', 'multi', 'normal', 'subarachnoid', 'subdural']
+    
+    # Which class we are isolating with this binary classifier
+    binary_classifier_type = 'subdural'
+
+    labelList = []
+    for ii in range(len(label_types)):
+        num_samples = 0
+        for r, d, files in os.walk(file_dir + label_types[ii]):
+            num_samples = len(files) 
+        if label_types[ii]==binary_classifier_type:
+            binaryLabel = 1
+        else:
+            binaryLabel = 0
+        listToAdd = [binaryLabel] * num_samples
+        labelList = labelList + listToAdd
+    print(len(labelList))
+
+    # https://stackoverflow.com/questions/52887227/understanding-follow-links-argument-in-kerass-imagedatagenerator
     training_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-        file_dir, image_size=(512, 512), batch_size=64, validation_split=0.2,
+        file_dir, labels = labelList, image_size=(512, 512), batch_size=64, validation_split=0.2,
         subset="training", seed=123, label_mode="binary",
     )
+    
 
     validation_dataset = tf.keras.preprocessing.image_dataset_from_directory(
         file_dir, image_size=(512, 512), batch_size=64, validation_split=0.2,
